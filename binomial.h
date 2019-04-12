8923f66a93b2ee30eb011f9de9b36d7109933849
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-04-11 19:21:20 -0500

    Update binomial.h

--- binomial.h
+++ binomial.h
@@ -3,11 +3,19 @@ static long numCalls = 0;
 //returns the number of function calls that were necessary to execute Pascal's Rule in choosing
 long getNumCalls();
 
-//TO DOCUMENT
+/**
+ * Runs Pascal's version of
+ * binomial coefficient
+ **/
 long choose(int n, int k);
 
 //Allocates memory for its subsequent recusive call of chooseWithMemoizationRecursive
 long chooseWithMemoization(int n, int k);
 
-//TO DOCUMENT
+/**
+ * Runs Pascal's version of
+ * binomial coefficient
+ * while also storing the values
+ * previously computed
+ **/
 long chooseWithMemoizationRecursive(int n, int k, long **tableau);
