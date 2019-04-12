4df965520fc7e4a5aaffa7cc285feb4e23a380ac
tylers1st <47163941+tylers1st@users.noreply.github.com>
2019-04-11 19:21:05 -0500

    Update binomialDemo.c

--- binomialDemo.c
+++ binomialDemo.c
@@ -1,4 +1,9 @@
-//DOCUMENTATION HERE
+/*
+ * Author: Tyler Furst
+ * Date: 04/11/2019
+ * This program is where all of the
+ * functions are stored for binomialDemo.c to run
+ */
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,12 +15,13 @@ int main(int argc, char **argv) {
   int n;
   int k;
 
+  scanf("%d %d", &n, &k);
   //TODO - scan n and k
 
   long choice = -1;
 
   //eventually modify to call memoized choice function
-  choice = choose(n, k);
+  choice = chooseWithMemoization(n,k);
 
   printf("choose(%d, %d) = %ld\n", n, k, choice);
   printf("number of calls: %ld\n", getNumCalls());
